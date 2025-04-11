a9b840b434582c920b4d7e2d5196959d125a2ab1
Ray <a0953295757@gmail.com>
2025-04-11 11:35:33 +0800

    我是master

--- SQL50/Program.cs
+++ SQL50/Program.cs
@@ -594,3 +594,5 @@ HAVING SUM(B.unit) >= 100
 SELECT user_id, name, mail FROM Users
 WHERE mail LIKE '[a-zA-Z]%@leetcode.com' AND LEFT(mail,LEN(mail)-13) NOT LIKE '%[^a-zA-Z0-9_.-]%'
 
+//我是master
+
